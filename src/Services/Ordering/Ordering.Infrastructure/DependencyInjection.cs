69331e109ccf5b19b8f942171841ad67c8ded3a9
Bogdan Gantu <gantubogdan@gmail.com>
2025-10-16 08:56:47 +0300

    adding domain related logic and models

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services, 
+            IConfiguration configuration)
+        {
+            var connString = configuration.GetConnectionString("Database");
+            //services.AddDbContext<ApplicationDbContext>(opts =>
+            //{
+            //    opts.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+            //});
+
+            //service.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            return services;
+        }
+    }
+}
