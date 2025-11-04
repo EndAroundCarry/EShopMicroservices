a1d6adb2e4226580ca2bcec27320bd46d87e4cfe
Bogdan Gantu <gantubogdan@gmail.com>
2025-11-04 08:46:49 +0200

    adding exception handler to program cs and Endpoints for ordering API

--- src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
+++ src/BuildingBlocks/BuildingBlocks/Exceptions/Handler/CustomExceptionHandler.cs
@@ -3,11 +3,6 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuildingBlocks.Exceptions.Handler
 {
